aec4ea3bcab020a5639789427de9d6f40935ac6e
yuhb <2656746362@qq.com>
2022-07-18 23:14:37 +0800

    second commit

--- src/main/java/com/yaxin/git/GitTest.java
+++ src/main/java/com/yaxin/git/GitTest.java
@@ -5,5 +5,6 @@ import java.util.Arrays;
 public class GitTest {
     public static void main(String[] args){
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
